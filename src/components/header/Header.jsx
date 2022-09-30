637130d70e932a2197b9de8ce55d3d00276fd0d7
Kraig Hebert <kraighebert@gmail.com>
2022-09-29 22:31:52 -0400

    added user avatar and a check for isAdmin to display Admin

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Avatar, Chip, Container, Typography } from '@mui/material';
 import { selectActiveUser } from '../../reducers/usersSlice';
 import * as SX from './headerSX';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
   const user = useSelector(selectActiveUser);
-  return <Container align="right">{user.firstName}</Container>;
+  return (
+    <Container align="right" maxWidth={false} sx={SX.containerSX}>
+      <Typography variant="h6">{user.isAdmin && 'Admin'}</Typography>
+      <Avatar variant="square" sx={SX.avatarSX} onClick={() => {}}>
+        {user.firstName[0]}
+      </Avatar>
+    </Container>
+  );
 };
 
 export default Header;
---
+++ src/components/header/headerSX.js
@@ -0,0 +1,17 @@
+export const containerSX = {
+  width: '90%',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+};
+
+export const avatarSX = {
+  backgroundColor: 'primary.main',
+  color: 'secondary.main',
+  marginLeft: '20px',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: 'primary.bg',
+    color: 'primary.main',
+  },
+};
